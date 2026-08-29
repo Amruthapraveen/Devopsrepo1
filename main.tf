0c4c47d08873445ba672e5cd4fc9dece244cf1bb
Amruthapraveen <152784930+Amruthapraveen@users.noreply.github.com>
2026-08-29 23:00:57 +0530

    Change default instance_type to t2.micro to avoid instance-type eligibility errors

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ variable "aws_region" {
 variable "instance_type" {
 	description = "EC2 instance type."
 	type        = string
-	default     = "t3.micro"
+	default     = "t2.micro"
 }
 
 variable "key_name" {
