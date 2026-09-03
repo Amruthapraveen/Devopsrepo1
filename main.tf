e8efb5a4c206793d1c70e8d55b5022804c0498e7
Amruthapraveen <152784930+Amruthapraveen@users.noreply.github.com>
2026-09-03 20:13:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-01a00762f46d584a1" # replace this
-  instance_type_value = "t2.micro"
+  instance_type_value = "t3.micro"
   subnet_id_value = "subnet-0011c47ae6be75de9". # replace this
 }
